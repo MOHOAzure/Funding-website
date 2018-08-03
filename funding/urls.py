5530f871374444d360f1b9e1a84489eec1c4f5d0
MOHOAzure <timecrutramer@gmail.com>
2018-08-03 16:21:21 +0800

    change name of index

--- funding/urls.py
+++ funding/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'funding'
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.TeamList.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<team_id>[0-9]+)/fund/$', views.fund, name='fund'),
 ]
