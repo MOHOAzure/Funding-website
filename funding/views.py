1676b9e63a51613e4b8e2881751156a4f7214075
MOHOAzure <timecrutramer@gmail.com>
2018-08-05 00:14:06 +0800

    fund a specific team

--- funding/views.py
+++ funding/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
 
@@ -14,6 +14,14 @@ class DetailView(generic.DetailView):
     model = Team
     template_name = 'funding/detail.html'
     
-def fund(request, team_id):
-    team = get_object_or_404(Team, pk=team_id)
-    return HttpResponseRedirect(reverse('funding:index'))
+def fund(request):
+    if request.is_ajax():
+        team_id = request.POST.get('team_id')
+        fund_to_be_add = request.POST.get('fund')
+        team = Team.objects.get(pk=team_id)
+        team.funds += int(fund_to_be_add)
+        team.save()
+        return HttpResponse('OK')    
+    else:
+        return HttpResponse('BAD')
+      
