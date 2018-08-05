1cc9edccd3da67e62bdfd2d99796e4477a51546d
MOHOAzure <timecrutramer@gmail.com>
2018-08-05 18:12:38 +0800

    rename fields

--- funding/models.py
+++ funding/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 class Team(models.Model):
     project = models.CharField(max_length=50)
-    funds = models.PositiveSmallIntegerField(default=0)    
+    funds = models.PositiveSmallIntegerField(default=0)
     def __str__(self):
         return self.project
     
-class DBSEMember(models.Model):
+class Funder(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    gold = models.PositiveSmallIntegerField(default=10)
+    budget = models.PositiveSmallIntegerField(default=10)
     def __str__(self):
         return self.user.username
