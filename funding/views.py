88bc539ec35c1e201c2dbdc6e04f4157566428d1
MOHOAzure <timecrutramer@gmail.com>
2018-08-05 18:11:56 +0800

    a user can fund a project according to his budget

--- funding/views.py
+++ funding/views.py
@@ -3,12 +3,17 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
-from .models import Team
+from .models import Team, Funder
 
 class TeamList(LoginRequiredMixin, generic.ListView):
     template_name = 'funding/index.html'
     queryset = Team.objects.order_by('project')
     context_object_name = 'team_list'
+    
+    def get_context_data(self, **kwargs):
+        context = super(TeamList, self).get_context_data(**kwargs)
+        context['user'] = Funder.objects.get(user=self.request.user)
+        return context
     
 @login_required(login_url='/accounts/login/')
 def fund(request):
@@ -18,6 +23,9 @@ def fund(request):
         team = Team.objects.get(pk=team_id)
         team.funds += int(fund_to_be_add)
         team.save()
+        user = Funder.objects.get(user=request.user)
+        user.budget -= int(fund_to_be_add)
+        user.save()
         return HttpResponse('OK')    
     else:
         return HttpResponse('BAD')
