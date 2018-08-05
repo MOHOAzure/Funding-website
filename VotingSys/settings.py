fbdaaa631aac118a6effe29e935313b05cde089f
MOHOAzure <timecrutramer@gmail.com>
2018-08-05 14:40:50 +0800

    add login

--- VotingSys/settings.py
+++ VotingSys/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'VotingSys.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +67,9 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/funding/'
 
 WSGI_APPLICATION = 'VotingSys.wsgi.application'
 
