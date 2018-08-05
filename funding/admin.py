82de357c31b9c6f7feb1e717c269e8c165d48bc5
MOHOAzure <timecrutramer@gmail.com>
2018-08-05 16:18:28 +0800

    Update admin.py

--- funding/admin.py
+++ funding/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import Team
-from .models import DBSEMember
+from .models import Funder
 
 admin.site.register(Team)
-admin.site.register(DBSEMember)
+admin.site.register(Funder)
