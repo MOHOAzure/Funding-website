1319681aad5290fdd812fb649b25c977743cecba
MOHOAzure <timecrutramer@gmail.com>
2020-10-04 16:58:33 +0800

    Update urls.py

--- VotingSys/urls.py
+++ VotingSys/urls.py
@@ -1,29 +1,18 @@
 """VotingSys URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 """
-from django.conf.urls import include, url
+from django.urls import include, path, reverse_lazy
 from django.contrib import admin
-from django.urls import reverse_lazy
 from django.contrib.auth.views import(
     LoginView,
     LogoutView,
 )
 
 urlpatterns = [
-    url(r'^funding/', include('funding.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^login/$', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    url(r'^logout/$', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout')
+    path('', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
+    path('funding/', include('funding.urls')),
+    path('admin/', admin.site.urls),
 ]
